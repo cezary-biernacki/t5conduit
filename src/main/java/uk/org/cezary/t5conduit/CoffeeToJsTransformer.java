7c3afc16217ef11167a52011a2ec2ba201a32c96
Cezary Biernacki <cezary.bi@gmail.com>
2011-11-10 18:15:45 +0100

    comment fixes

--- src/main/java/uk/org/cezary/t5conduit/CoffeeToJsTransformer.java
+++ src/main/java/uk/org/cezary/t5conduit/CoffeeToJsTransformer.java
@@ -1,4 +1,4 @@
-// Copyright 2011 Cezary Biernacki., Licensed under the Apache License, Version 2.0 (the "License").
+// Copyright 2011 Cezary Biernacki. Licensed under the Apache License, Version 2.0 (the "License").
 //
 // You may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
--- src/main/java/uk/org/cezary/t5conduit/LessToCssTransformer.java
+++ src/main/java/uk/org/cezary/t5conduit/LessToCssTransformer.java
@@ -1,7 +1,6 @@
-// Copyright 2011 The Apache Software Foundation
+// Copyright 2011 Cezary Biernacki. Licensed under the Apache License, Version 2.0 (the "License").
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// You may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 // http://www.apache.org/licenses/LICENSE-2.0
